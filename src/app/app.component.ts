7c7243f9473cd4ccb5ac233753b9efeb21c25fc2
averge <48928310+averge@users.noreply.github.com>
2022-11-08 14:56:37 -0300

    tipo 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'previapp';
-  tiposDePregunta=[{id:1, descripcion:"dos involucrados"}, {id:2, descripcion:"Todos participan"}]
+  tiposDePregunta=[{id:1, descripcion:"dos involucrados"}, {id:2, descripcion:"Todos participan"}, {id:3, descripcion:"Un participante"}]
   participantes:any=[{nombre:"Loren"},{nombre:"Pedro"}, {nombre:"Willy"}, {nombre:"Fede"}];
-  preguntas:any=[{pregunta:"se la chupa a", tipo:1}, {pregunta:"se coge a la mama de", tipo:1},{pregunta:"Quien la tiene mas grande?", tipo:2}];
+  preguntas:any=[{pregunta:"se la chupa a", tipo:1}, {pregunta:"se coge a la mama de", tipo:1},{pregunta:"Quien la tiene mas grande?", tipo:2},{pregunta:"Quien se la chupo a quien?", tipo:2}, {pregunta:"Toma 5 tragos", tipo:3},{pregunta:"Hace un baile sensual", tipo:3}, {pregunta:"le da un beso a", tipo:1}, {pregunta:"Chupala me canse de pesnar preguntas!!", tipo:3}];
   nuevaPersona = new FormGroup(
     {nombre : new FormControl('', [Validators.required])}
   )
@@ -41,8 +41,12 @@ export class AppComponent {
     let randomPregunta = Math.floor(Math.random() * this.preguntas.length);
     if (this.preguntas[randomPregunta].tipo==1){
       this.pregunta=nombre1 + " " + this.preguntas[randomPregunta].pregunta + " " + this.participantes[randomParticipante2].nombre
-    }else{
-      this.pregunta= this.preguntas[randomPregunta].pregunta
+    }
+    if (this.preguntas[randomPregunta].tipo==2){
+      this.pregunta=this.preguntas[randomPregunta].pregunta
+    }
+    else{
+      this.pregunta= nombre1 + " " + this.preguntas[randomPregunta].pregunta
     }
   }
 }
