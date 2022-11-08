e64b71fe6b92e2434a8b4e757043698f03691d78
averge <48928310+averge@users.noreply.github.com>
2022-11-08 14:31:53 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'previapp';
+
+  participantes:any=[{nombre:"Loren"},{nombre:"Pedro"}, {nombre:"Willy"}, {nombre:"Fede"}];
+  preguntas:any=[{pregunta:"se la chupa a"}, {pregunta:"se coge a la mama de"}];
+  nuevaPersona = new FormGroup(
+    {nombre : new FormControl('', [Validators.required])}
+  )
+  nuevaPregunta= new FormGroup(
+    {pregunta : new FormControl('', [Validators.required])}
+  )
+  pregunta=''
+
+
+  addPersona(){
+      this.participantes.push(this.nuevaPersona.value);
+    
+    this.nuevaPersona.reset();
+  }
+
+  addPregunta(){
+    this.preguntas.push(this.nuevaPregunta.value);
+    this.nuevaPregunta.reset();
+  }
+  random(){
+    let randomParticipante1 = Math.floor(Math.random() * this.participantes.length);
+    let nombre1 =this.participantes[randomParticipante1].nombre
+    let randomParticipante2 = Math.floor(Math.random() * this.participantes.length);
+    while(randomParticipante1==randomParticipante2){
+      randomParticipante2 = Math.floor(Math.random() * this.participantes.length);
+    }
+    let randomPregunta = Math.floor(Math.random() * this.preguntas.length);
+    this.pregunta=nombre1 + " " + this.preguntas[randomPregunta].pregunta + " " + this.participantes[randomParticipante2].nombre
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
